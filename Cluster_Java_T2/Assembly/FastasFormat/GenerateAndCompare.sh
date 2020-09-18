5b940d9de11f72fd3290af0b62e871ce9dabb975
DavidRFB <dr.figueroa10@uniandes.edu.co>
2020-09-18 13:46:20 -0500

    FaltaBono

--- Cluster_Java_T2/Assembly/FastasFormat/GenerateAndCompare.sh
+++ Cluster_Java_T2/Assembly/FastasFormat/GenerateAndCompare.sh
@@ -1,4 +1,5 @@
 ### generate fasta files with lines of 70 b per line
 for i in ../COVID*; do fold -w70 $i > ${i:3:25}.fasta ; done
-
+#compare
+ for i in COVID*; do echo $i; diff -U 0 ReferenceCovid.fa $i | grep -v ^@ | tail -n +3 | wc -l ; done
 
