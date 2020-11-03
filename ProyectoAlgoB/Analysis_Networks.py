1aa486669be6fac399c718288d836a5ed3b731da
DavidRFB <dr.figueroa10@uniandes.edu.co>
2020-11-03 18:15:36 -0500

    falta Analysis_Networks

---
+++ ProyectoAlgoB/Analysis_Networks.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  3 17:32:31 2020
+
+@author: David Ricardo Figueora 
+@email: dr.figueroa10
+
+DESCRIPTION 
+         A textual description of the functioning of the command or function. "
+EXAMPLES 
+         Some examples of common usage. 
+SEE ALSO 
+         A list of related commands or functions. "
+BUGS 
+         List known bugs. 
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt 
+import os 
+import sys 
+import re
+
+def nodes_degree(graph):
+    # graficar distribucion de vertices. 
+    # parecido a los kmers
+    # return array index as degree value at index i is the total number of nodes.
+    
+def global_density(graph):
+    """
+    
+
+    Parameters
+    ----------
+    graph : graph
+        LJ parameters.
+
+    Returns
+    -------
+    global_density of graph.
+
+    """
+    
+    
