4c87e0be062d03a412d5e14b642327ad2105ee56
DavidRFB <dr.figueroa10@uniandes.edu.co>
2020-11-07 16:56:04 -0500

    falta arreglar error164

--- ProyectoAlgoB/main.py
+++ ProyectoAlgoB/main.py
@@ -27,9 +27,9 @@ Created on Wed Oct  7 14:45:45 2020
 
 """
 
-#import numpy as np
+import numpy as np
 import matplotlib.pyplot as plt
-#import os
+import os
 #import time
 #import TopologyReader as topr
 import Graph as G
@@ -43,13 +43,45 @@ import networkx as nx
 
 if __name__ == "__main__":
     #filename = "water_prot"
-    filename = "back_wter_pro"
-
-    with open("./{}.pdb".format(filename),"r") as pdbfile:
-        content = pdbfile.readlines()
-    G_class = G.Graph( content )
-    distance_graph = G_class.createEdges ( G_class.cartesian, 16 )
-    lj_graph = G_class.createEdges( G_class.LJ, 15 )
+    
+    path="/home/david/Documents/BionIF/Algortimos/Proyecto/MD/MD_dataset/"
+    pdbs = os.listdir(path)
+    for  i in pdbs[2:3]:
+        if(len(i)<5):
+            print(i)
+            mutants = os.listdir(path+i)
+            for mutval in mutants:
+                results=[]
+                print(path+i+"/"+mutval)
+                files_per_mut = os.listdir(path+i+"/"+mutval)
+                data_energy=np.loadtxt(path+i+"/"+mutval+"/energy.xvg",comments=["@","#"])
+                for files_dyn in files_per_mut:
+                    file_nm = files_dyn.split("_")
+                    if(".pdb" in files_dyn and len(file_nm)>3):
+                        frame=int(file_nm[1])/3
+                        mut_coord = file_nm[2]
+                        new_aa = mut_coord[0]
+                        chain = mut_coord[1]
+                        pos_mut = mut_coord[2:-1]
+                        old_aa = mut_coord[-1]
+                        with open(path+i+"/"+mutval+"/"+files_dyn,"r") as pdbfile:
+                            content = pdbfile.readlines()
+                        G_class = G.Graph( content )
+                        distance_graph = G_class.createEdges ( G_class.cartesian, 16 )
+                        lj_graph = G_class.createEdges( G_class.LJ, 15 )
+                        global_density = antx.global_density( lj_graph )
+                        loca_densitu = antx.local_densities(lj_graph,pos_mut)
+                        
+                        
+                        
+                        #results.append(data_energy[int(frame)][0],data_energy[int(frame)][1:])
+    #filename = "back_wter_pro"
+    
+    #with open("./{}.pdb".format(filename),"r") as pdbfile:
+    #    content = pdbfile.readlines()
+    #G_class = G.Graph( content )
+    #distance_graph = G_class.createEdges ( G_class.cartesian, 16 )
+    #lj_graph = G_class.createEdges( G_class.LJ, 15 )
     #water_graph = G_class.createEdges( G_class.water, 15 )
     print()
 
